0829a120af52325a8356417d4307854c01ba457d
praveentimbu <113846922+praveentimbu@users.noreply.github.com>
2022-11-10 08:58:12 +0530

    Delete preprocessing.py

--- Data Collection/image processing/preprocessing.py
+++
@@ -1,5 +0,0 @@
-from keras.preprocessing.image import ImageDataGenerator
-train_datagen=ImageDataGenerator(rescale=1./255,shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
-test_datagen = ImageDataGenerator(rescale=1./255)
-x_train=train_datagen.flow_from_directory( 'C:\Dataset\train',target_size=(64,64),batch_size=5,color_mode='grayscale',class_mode='categorical')
-x_test=test_datagen.flow_from_directory( 'C:\Dataset\test',target_size=(64,64),batch_size=5,color_mode='grayscale',class_mode='categorical')
